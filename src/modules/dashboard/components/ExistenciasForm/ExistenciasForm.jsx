da98637e6af8c544542c306f005d3b27be6617d9
unknown <pierodavilaaguirre22@gmail.com>
2023-09-20 22:52:29 -0500

    work in progress...

--- src/modules/dashboard/components/ExistenciasForm/ExistenciasForm.jsx
+++ src/modules/dashboard/components/ExistenciasForm/ExistenciasForm.jsx
@@ -4,12 +4,14 @@ import ButtonF from "../Button/Button"
 import Styles from "./ExistenciasForm.module.css"
 import { useEffect, useState } from "react"
 import ErrorForm from "../../../auth/components/ErrorForm/ErrorForm"
+
 const InputCodigoProps = {
     id: 'codigo',
     name: 'codigo',
     placeholder: 'ingresa el codigo',
     type: 'number',
-    label: 'Codigo'
+    label: 'Codigo',
+    disabled: true
 }
 const InputAnaquelProps = {
     id: 'id_almacen',
@@ -47,41 +49,29 @@ const InputCantidadProps = {
     label: 'Cantidad'
 }
 
-export default function ExistenciasForm( {id, action} ) {
-    const [producto, setProducto] = useState( {id_inventario:null,id_almacen:null,descripcion:null,cantidad:null} )
-    
+export default function ExistenciasForm({ id, action }) {
+    const [producto, setProducto] = useState({ id_almacen: "", descripcion: "", cantidad: "" })
+
     useEffect(() => {
-        const fetchProduct = async () => { 
+        const fetchProduct = async () => {
             const url = `http://localhost:3000/products/${id}`
             const recibir = await fetch(url)
             const response = await recibir.json()
             setProducto(response);
         }
-         if(id && action==='editar'){
-            
-           fetchProduct()
-         }else{
-            setProducto({id_inventario:null,id_almacen:null,descripcion:null,cantidad:null});
-         }
-    },[id])
 
-    useEffect(() => {
-        if (action === 'añadir') {
-            setProducto({
-                codigo: '',
-                descripcion: '',
-                id_almacen: '',
-                cantidad: '',
-                hasErrors: false
-            });
+        if (id && action === 'editar') {
+            fetchProduct()
+        } else {
+            setProducto({ id_almacen: "", descripcion: "", cantidad: "" });
         }
-    }, [action]);
+    }, [id])
+
 
     const handleAñadir = (event) => {
         setProducto((prev) => ({ ...prev, [event.target.id]: event.target.value }))
     }
 
-    
 
     const handleSubmit = async (event) => {
         event.preventDefault();
@@ -94,41 +84,45 @@ export default function ExistenciasForm( {id, action} ) {
         setProducto(prev => ({ ...prev, hasErrors: false }))
 
         const newProducto = {
-            codigo: `${producto.codigo}`,
             descripcion: producto.descripcion,
             id_almacen: producto.id_almacen,
             cantidad: producto.cantidad,
         };
 
-        const request = await fetch('http://localhost:3000/products', {
-            headers: {
-                'Content-type': 'application/json',
-            },
-            method: 'POST',
-            body: JSON.stringify({
-                ...newProducto
+
+        if (action === 'añadir') {
+            const request = await fetch('http://localhost:3000/products', {
+                headers: {
+                    'Content-type': 'application/json',
+                },
+                method: 'POST',
+                body: JSON.stringify({
+                    ...newProducto
+                })
             })
-        })
-
-        const response = await request.json()
-
-        if (!response.ok) {
-            return
+        } else {
+            // TODO:
+            // haces literalmente lo mismo que añadir, unicamente cambias la url
+            // bad request - revisa que la data que le estes enviando sea la misma que el backend esta esperando.
+            // error 500 - revisa que la data que le estes enviando al backend le este llegando correctamente
+            // method not supported - ESTO SIGNIFICA QUE EL METODO DEL FETCH NO ES COMPATIBLE CON EL METODO HTTP QUE ESTA ESPERANDO TU ENDPOINT
         }
 
-        localStorage.setItem('token', response.user.token);
-        navigate('/dashboard/inventory/existencias')
+        location.reload();
     }
-   //tener un handler, tener un if, si action es editar usar las cosas para editar, si action es añadir usar la api añadir
     return (
-           <Form styling={{ width: '100%' }} onSubmit={handleSubmit} >
+        <Form styling={{ width: '100%' }} onSubmit={handleSubmit} >
             <div>
-                <div>
-                    <InputF value={producto.id_inventario }  {...InputCodigoProps} onChange={handleAñadir} />
-                </div>
+                {
+                    action === 'editar' && (
+                        <div>
+                            <InputF {...InputCodigoProps} />
+                        </div>
+                    )
+                }
 
                 <div>
-                    <InputF value={producto.descripción} {...InputDescripcionProps} onChange={handleAñadir}  />
+                    <InputF value={producto.descripcion} {...InputDescripcionProps} onChange={handleAñadir} />
                 </div>
 
                 <div>
@@ -144,17 +138,16 @@ export default function ExistenciasForm( {id, action} ) {
                 </div>
 
                 <div>
-                    <InputF value={producto.cantidad}  {...InputCantidadProps} onChange={handleAñadir}  />
+                    <InputF value={producto.cantidad}  {...InputCantidadProps} onChange={handleAñadir} />
                 </div>
                 <div className={Styles.FormFooter}>
                     <ButtonF text='Añadir' />
                     <ButtonF text='Cancelar' />
-
                 </div>
             </div>
             {
-                    producto.hasErrors && <ErrorForm msg={"Rellena todos los campos"} />
+                producto.hasErrors && <ErrorForm msg={"Rellena todos los campos"} />
             }
-        </Form> 
+        </Form>
     )
 }
